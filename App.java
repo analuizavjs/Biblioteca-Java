8065e5e61b24429adb3fa137bb894dd22157b45c
PedroHCastilhos <pedro.castilhos.fraga@gmail.com>
2025-06-29 12:29:19 -0300

    Espero que não dê bosta no app

--- App.java
+++ App.java
@@ -1,10 +1,6 @@
 public class App {
     public static void main(String[] args) {
-<<<<<<< HEAD
         ClubeDoLivro clube = new ClubeDoLivro();
-        System.out.println(clube.grade()); 
-=======
-        
->>>>>>> origin/classe-clube-do-livro
+        System.out.println(clube.grade());
     }
 }
