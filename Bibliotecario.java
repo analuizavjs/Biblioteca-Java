bae796d956fac2f69f4f793fd8703805f8f81331
Lais Barbizan <laisbzn@hotmail.com>
2025-06-30 16:29:31 -0300

    Comentarios Bibliotecario

--- Bibliotecario.java
+++ Bibliotecario.java
@@ -1,14 +1,18 @@
+// Declaração da classe Bibliotecario
 public class Bibliotecario {
+    //Atributos
     private String nome;
     private int idade;
     private String cpf;
 
+    //Construtor 
     public Bibliotecario(String nome, int idade, String cpf) {
         this.nome = nome;
         this.idade = idade;
         this.cpf = cpf;
     }
 
+    //Métodos getters e setters 
     public String getBibNome() {
         return this.nome;
     }
@@ -30,7 +34,9 @@ public class Bibliotecario {
         this.cpf = cpf;
     }
 
+    // Método que retorna uma representação em texto formatado do bibliotecário
     public String toBibString() {
+        // Retorna uma string com os dados do bibliotecário formatados
         return String.format("Nome: %-30s | Idade: %-3d | CPF: %-11s", this.nome, this.idade, this.cpf);
     }
 }
